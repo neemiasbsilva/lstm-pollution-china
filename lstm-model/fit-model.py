580b954813f28140b20cadff8f71d9c7c18ef247
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-14 11:22:33 -0400

    split the dataset into train, test & fit the model

--- lstm-model/fit-model.py
+++ lstm-model/fit-model.py
@@ -4,3 +4,11 @@ import pandas as pd
 # load the dataset
 dataset = pd.read_csv("../pollution.csv", header=0, index_col=0)
 
+# split into train and test
+values = dataset.values
+
+n_train_hours = 365 * 24
+
+train = values[:n_train_hours,:]
+test = values[n_train_hours:, :]
+
