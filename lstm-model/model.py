e20ac2048a17a47028243c4fd9cca9f3eb8652d9
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-14 11:08:05 -0400

    define lstm model with 1 LSTM hidden layer and 1 layeer dense as output

---
+++ lstm-model/model.py
@@ -0,0 +1,11 @@
+from keras.models import Sequential
+from keras.layes import Dense, LSTM
+
+def get_train_model(x_train):
+
+    model = Sequential()
+
+    model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2])))
+    model.add(Dense(1))
+
+    return model
