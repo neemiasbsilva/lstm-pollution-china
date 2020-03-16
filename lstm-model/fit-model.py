b9d149382543f4a198d1d610eba1526b614e78cd
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-16 00:01:27 -0400

    split the dataset into train, test & fit the model

--- lstm-model/fit-model.py
+++ lstm-model/fit-model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from model import get_train_model
-
+from sklearn.model_selection import train_test_split
 # load the dataset
 dataset = pd.read_csv("../pollution.csv", header=0, index_col=0)
 
@@ -24,3 +24,9 @@ x_test = x_test.reshape((x_test.shape[0], 1, x_test.shape[1]))
 model = get_train_model(x_train)
 
 model.compile(loss='mse', optimizer='SGD')
+
+x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5, random_state=121)
+
+model.fit(x_train, y_train, epochs=100, batch_size=512, validation_data=(x_val, y_val), verbose=2, shuffle=False)
+
+model.save("../model_final.h5")
